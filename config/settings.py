72b3d71ad212e8436944ac80b1aebc853bd9cc6e
ReverseCall <110948768+ReverseCall@users.noreply.github.com>
2025-06-06 00:20:54 -0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-myyt!*_ffr_5%nlp45@w()8khjvz(v_!q@c^y0t=lv0c_b2e)o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
